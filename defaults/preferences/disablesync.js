cd441e6a2fec3de0883e324bac897bedbc465fd9
SHIMODA Hiroshi <shimoda@clear-code.com>
2012-04-06 14:31:14 +0900

    Disable some preferences for sync

---
+++ defaults/preferences/disablesync.js
@@ -0,0 +1,7 @@
+
+pref("services.sync.serverURL", "");
+pref("services.sync.jpake.serverURL", "");
+pref("services.sync.termsURL", "");
+pref("services.sync.privacyURL", "");
+pref("services.sync.statusURL", "");
+pref("services.sync.syncKeyHelpURL", "");
