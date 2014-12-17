b9016f5520bfb46c89af45930841c0940937b96c
YUKI Hiroshi <shimoda@clear-code.com>
2014-12-17 13:08:37 +0900

    Disable more prefs for Firefox Sync

--- defaults/preferences/disablesync.js
+++ defaults/preferences/disablesync.js
@@ -2,9 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-pref("services.sync.serverURL", "");
+pref("identity.fxaccounts.auth.uri", "");
+pref("identity.fxaccounts.remote.force_auth.uri", "");
+pref("identity.fxaccounts.remote.signin.uri", "");
+pref("identity.fxaccounts.remote.signup.uri", "");
+pref("identity.fxaccounts.settings.uri", "");
+pref("services.sync.fxa.privacyURL", "");
+pref("services.sync.fxa.termsURL", "");
 pref("services.sync.jpake.serverURL", "");
-pref("services.sync.termsURL", "");
 pref("services.sync.privacyURL", "");
+pref("services.sync.serverURL", "");
 pref("services.sync.statusURL", "");
 pref("services.sync.syncKeyHelpURL", "");
+pref("services.sync.termsURL", "");
